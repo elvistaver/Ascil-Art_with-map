b9c77124a14678317bdbbb3cdc099c8f074ea733
elvistaver <elvistaver@gmail.com>
2026-05-06 00:12:09 +0100

    map version

--- render.go
+++ render.go
@@ -5,16 +5,14 @@ import (
 	"strings"
 )
 
-func Renderline(font map[rune][]string) string {
-	result := ""
-
+func Renderline(font map[rune][]string) []string {
+	result :=[]string{}
 	input := strings.TrimSpace(os.Args[1])
 	input1 := Split(input)
 
-
 	for _, ch := range input1 {
-		if ch =="" {
-			result+="\n" 
+		if len(result)>0 && ch=="" {
+			result=append(result,"") 
 			continue
 		}
 		for i := 0; i < 8; i++ {
@@ -22,7 +20,7 @@ func Renderline(font map[rune][]string) string {
 			for _, char := range ch {
 				line+= font[char][i]
 			}
-			result +=line+"\n"
+			result=append(result, line+"\n")
 		}
 	}
 	return result
